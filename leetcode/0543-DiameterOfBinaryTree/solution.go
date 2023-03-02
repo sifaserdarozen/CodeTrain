4cb8632af4d6df63eff690df041287c0ae1edd3b
sifa serdar ozen <sifa.serdar.ozen@gmail.com>
2023-03-02 21:48:21 +0100

    Add leetcode problem Diameter of Binary Tree
    Compute Complexity: O(N)
    Space Complexity  : O(1)
    
    543.Diameter of Binary Tree
    https://leetcode.com/problems/diameter-of-binary-tree/description/

---
+++ leetcode/0543-DiameterOfBinaryTree/solution.go
@@ -0,0 +1,42 @@
+package solution
+
+// Compute Complexity = O(N)
+// Space Complexity = O(N)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+ func getMax(nums ...int) int {
+    max := nums[0]
+    for _, v := range(nums) {
+        if v > max {
+            max = v
+        }
+    }
+    return max
+}
+
+func getDiameter(node *TreeNode)(int, int) {
+    if nil == node {
+        return 0, 0
+    }
+
+    leftMaxDepth, leftMaxDiameter := getDiameter(node.Left)
+    rightMaxDepth, rightMaxDiameter := getDiameter(node.Right)
+
+    maxDepth := 1 + getMax(leftMaxDepth, rightMaxDepth)
+    diameter := leftMaxDepth + rightMaxDepth
+    maxDiameter := getMax(leftMaxDiameter, rightMaxDiameter, diameter)
+
+    return maxDepth, maxDiameter    
+}
+func diameterOfBinaryTree(root *TreeNode) int {
+    _, maxDiameter := getDiameter(root)
+    return maxDiameter
+}
